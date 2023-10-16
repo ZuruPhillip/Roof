2690146d4251390f6bdee85ff19a86bf57e3cb76
phillip <hustphillip@163.com>
2023-10-16 11:36:38 +0800

    完成book curd操作

--- src/ZURU.Roof.Application/RoofApplicationAutoMapperProfile.cs
+++ src/ZURU.Roof.Application/RoofApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ZURU.Roof.Books;
 
 namespace ZURU.Roof;
 
@@ -9,5 +10,7 @@ public class RoofApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
     }
 }
