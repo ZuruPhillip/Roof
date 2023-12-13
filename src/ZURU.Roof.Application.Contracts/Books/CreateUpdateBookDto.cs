b415c18b63bc73ff83d9591a61698f031c3e1eb2
ZuruPhillip <hustphillip@163.com>
2023-12-13 18:05:49 +0800

    更新Book验证逻辑

--- src/ZURU.Roof.Application.Contracts/Books/CreateUpdateBookDto.cs
+++ src/ZURU.Roof.Application.Contracts/Books/CreateUpdateBookDto.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ZURU.Roof.Books
 {
-    public class CreateUpdateBookDto
+    public class CreateUpdateBookDto : IValidatableObject
     {
         [Required]
         [StringLength(128)]
@@ -18,7 +18,13 @@ namespace ZURU.Roof.Books
         [DataType(DataType.Date)]
         public DateTime PublishDate { get; set; } = DateTime.Now;
 
-        [Required]
+        [Range(0, 999.99)]
         public float Price { get; set; }
+
+        //自定义验证逻辑
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
